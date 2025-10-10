05b38c8151ece3ac3b2ae6f64787635a05d93630
shornell chabalala <shornellchabalala60@gmail.com>
2025-10-10 20:42:44 +0200

    Update train_model.py
    model trained

--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,31 @@
+import pandas as pd 
+import numpy as np 
+import pickle
+from sklearn.metrics.pairwise import cosine_similarity 
+from data_preprocessing import load_and_clean_data
+
+
+#step1: train the model 
+def train_item_based_model():
+    df = load_and_clean_data()
+    print(f"✅ Data cleaned. Shape: {df.shape}")
+
+    #step2: Build user-item interaction matrix
+    user_item_matrix = df.pivot_table(
+        index="customer id", columns="description", values="quantity", aggfunc="sum", fill_value=0
+    )
+    print(f"User-Item Matrix shape: {user_item_matrix.shape}")
+
+    #step3: Compute item-based similarity
+    similarity = cosine_similarity(user_item_matrix.T)
+    print("✅ Item-based similarity matrix trained!")
+
+    #step4: Save both similarity AND user_item_matrix
+    with open("item_based_model.pkl", "wb") as f:
+        pickle.dump((similarity, user_item_matrix), f)
+
+    print("✅ Model saved as item_based_model.pkl")
+
+if __name__ == "__main__":
+    train_item_based_model()
 
