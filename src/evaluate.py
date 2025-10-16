81ee223fdfab463ed3e99d10c38a328e1ba0bd3d
Ndivho Daniel Rambauli <danielrambauli8@gmail.com>
2025-10-16 19:09:07 +0200

    Implement evaluate_model function in evaluate.py
    Add model evaluation function to load and retrieve data

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,9 @@
+import pickle
+
+def evaluate_model(file_path="item_based_model.pkl"):
+    with open(file_path, "rb") as f:
+        model_data = pickle.load(f)
+
+    user_item_matrix = model_data["user_item_matrix"]
+    similarity_matrix = model_data["similarity_matrix"]
 
